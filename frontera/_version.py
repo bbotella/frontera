35bcb7e52f70c9edd6f8a19c82f10bb5aff9826b
bbotella <contacto@bernardobotella.com>
2015-07-20 15:25:17 +0200

    test commit

--- frontera/_version.py
+++ frontera/_version.py
@@ -36,8 +36,8 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = ""
-    cfg.tag_prefix = "v"
-    cfg.parentdir_prefix = "frontera"
+    cfg.tag_prefix = ""
+    cfg.parentdir_prefix = "frontera-"
     cfg.versionfile_source = "frontera/_version.py"
     cfg.verbose = False
     return cfg
